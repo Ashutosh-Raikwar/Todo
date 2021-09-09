65a71916bef5c6dad766203900c73b51fe6f21ee
Ashutosh Raikwar <ashutosh>
2021-09-10 03:17:28 +0530

    improve the input of todo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,46 @@ class App extends React.Component{
     this.state={
       items: [],
       input:'',
-      onEditing: false,
+      isEditing: false,
     }
   }
+
+  onEditing = () => {
+    this.setState({isEditing: true})
+    }
+  onChange = e =>{
+    this.setState({input: e.target.value})
+  }
+  onClick =() =>{}
+  onSubmit = e => {
+    e.preventDefault();
+    this.setState({items: this.state.items.concat(this.state.input), input:''});
+  }
+  onDelete = () => {}
+
+  render(){
+    return(
+    <form id="todo-list" onSubmit={this.onSubmit}>
+      {this.state.items.length > 0?
+      this.state.items.map((item) => {
+        return(
+            <sapn className="todo-wrap">
+              <span>
+                <input type="checkbox" />
+                <label for="1" className="todo"><i className="fa fa-check"></i></label>
+              </span>
+              <span className="delete-item" title="remove">
+                <i className="fa fa-times-circle"></i>
+              </span>
+            </sapn>
+        )
+      }):<p></p>}
+      {this.state.isEditing ?<span className="todo-wrap"><input  onChange={this.onChange} value={this.state.input} /></span>:''}
+    <div id="add-todo" onClick={this.onEditing}><i className="fa fa-plus"></i> &nbsp; Add an item</div>
+    </form>
+    
+    )
+  }
 }
+
+export default App;
